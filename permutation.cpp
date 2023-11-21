62e0c85bcdb04bf7192cf8085f79d0fd0bfa688f
sahilyadav-1 <121487105+sahilyadav-1@users.noreply.github.com>
2023-11-21 06:34:08 +0530

    Create permutation.cpp

---
+++ permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    void solve(vector<int>& nums, int i, vector<vector<int>> &ans){
+        if(i == nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int j = i; j < nums.size(); j++){
+            cout<<"i  "<<i<<"  "<<"j   "<<j<<endl;
+            swap(nums[i],nums[j]);
+            solve(nums,i+1,ans);
+            swap(nums[i],nums[j]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        
+        solve(nums,0,ans);
+        
+        return ans;
+    }
+};
